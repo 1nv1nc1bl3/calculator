c0bd29c21fb9fc983673f9dfd9d014739b88ff18
Alkis <alkis.k1972@gmail.com>
2026-03-08 15:57:45 +0200

    Calculator ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,169 @@
 import { useState } from 'react';
 
 export default function App() {
+    // State
+    const [input, setInput] = useState('0');
+
     // Button Styles
-    const baseBtn =
-        'cursor-pointer text-2xl p-2 rounded-lg bg-slate-800 hover:bg-slate-700 btn';
+    const baseBtn = 'cursor-pointer text-2xl p-2 rounded-lg btn';
 
     const buttonStyles = {
-        number: '',
-        operator:
-            'cursor-pointer text-2xl p-2 rounded-lg bg-sky-950 hover:bg-sky-900 btn',
-        control:
-            'cursor-pointer text-2xl p-2 rounded-lg bg-stone-900 hover:bg-stone-800 btn',
-        equals: 'cursor-pointer text-2xl p-2 rounded-lg bg-gray-900 hover:bg-gray-800 btn',
+        number: 'bg-slate-800 hover:bg-slate-700',
+        operator: 'bg-sky-950 hover:bg-sky-900',
+        control: 'bg-stone-900 hover:bg-stone-800',
+        equals: 'bg-gray-900 hover:bg-gray-800',
     };
 
     // Functions
-    const [display, setDisplay] = useState('0');
-
-    const handleNumber = (num) => {
-        setDisplay((prev) => (prev === '0' ? num : prev + num));
-    };
-
-    const handleOperator = (op) => {
-        setDisplay((prev) => prev + ' ' + op + ' ');
+    const handleClick = (num) => {
+        setInput((prev) => (prev === '0' ? num : prev + num));
     };
 
     const handleClear = () => {
-        setDisplay('0');
+        setInput('');
     };
 
     const handleDelete = () => {
-        setDisplay((prev) => (prev.length === 1 ? '0' : prev.slice(0, -1)));
+        setInput((prev) => prev.slice(0, -1));
     };
 
-    const handleEquals = () => {
+    const handleCalculate = () => {
         try {
-            const result = eval(display.replace(/×/g, '*').replace(/÷/g, '/'));
-            setDisplay(String(result));
+            const result = eval(input);
+            setInput(result.toString());
         } catch {
-            setDisplay('Error');
+            setInput('Error');
         }
     };
 
     return (
         <div className='calculator-container font-display min-h-screen flex flex-col  justify-center items-center text-white bg-slate-900 gap-10'>
-            <h1 class='text-4xl mb-6 text-slate-300'>Calculator</h1>
+            <h1 className='text-4xl mb-6 text-slate-300'>Calculator</h1>
             <div className='calculator min-w-max flex flex-col justify-center items-center bg-slate-950 rounded-2xl p-8 shadow-xl shadow-slate-800/50 gap-4'>
                 <div
                     className='display w-full rounded-lg text-right bg-slate-700 text-4xl text-slate-300 px-2 py-4 overflow-x-auto
 '
                 >
-                    {display}
+                    {input || 0}
                 </div>
 
                 <div className='buttons grid grid-cols-4 gap-4'>
-                    <button className={`${buttonStyles.control} control`}>
+                    <button
+                        className={`${buttonStyles.control} ${baseBtn}`}
+                        onClick={handleClear}
+                    >
                         C
                     </button>
-                    <button className={`${buttonStyles.control} control`}>
+                    <button
+                        className={`${buttonStyles.control} ${baseBtn}`}
+                        onClick={handleDelete}
+                    >
                         DEL
                     </button>
-                    <button className={`${buttonStyles.operator} operator`}>
+                    <button
+                        className={`${buttonStyles.operator} ${baseBtn}`}
+                        onClick={() => handleClick('%')}
+                    >
                         %
                     </button>
-                    <button className={`${buttonStyles.operator} operator`}>
+                    <button
+                        className={`${buttonStyles.operator} ${baseBtn}`}
+                        onClick={() => handleClick('/')}
+                    >
                         /
                     </button>
 
-                    <button className={`${baseBtn} btn`}>7</button>
-                    <button className={`${baseBtn} btn`}>8</button>
-                    <button className={`${baseBtn} btn`}>9</button>
-                    <button className={`${buttonStyles.operator} operator`}>
+                    <button
+                        className={`${baseBtn} ${buttonStyles.number}`}
+                        onClick={() => handleClick('7')}
+                    >
+                        7
+                    </button>
+                    <button
+                        className={`${baseBtn} ${buttonStyles.number}`}
+                        onClick={() => handleClick('8')}
+                    >
+                        8
+                    </button>
+                    <button
+                        className={`${baseBtn} ${buttonStyles.number}`}
+                        onClick={() => handleClick('9')}
+                    >
+                        9
+                    </button>
+                    <button
+                        className={`${buttonStyles.operator} ${baseBtn}`}
+                        onClick={() => handleClick('*')}
+                    >
                         x
                     </button>
 
-                    <button className={`${baseBtn} btn`}>4</button>
-                    <button className={`${baseBtn} btn`}>5</button>
-                    <button className={`${baseBtn} btn`}>6</button>
-                    <button className={`${buttonStyles.operator} operator`}>
+                    <button
+                        className={`${baseBtn} ${buttonStyles.number}`}
+                        onClick={() => handleClick('4')}
+                    >
+                        4
+                    </button>
+                    <button
+                        className={`${baseBtn} ${buttonStyles.number}`}
+                        onClick={() => handleClick('5')}
+                    >
+                        5
+                    </button>
+                    <button
+                        className={`${baseBtn} ${buttonStyles.number}`}
+                        onClick={() => handleClick('6')}
+                    >
+                        6
+                    </button>
+                    <button
+                        className={`${buttonStyles.operator} ${baseBtn}`}
+                        onClick={() => handleClick('-')}
+                    >
                         -
                     </button>
 
-                    <button className={`${baseBtn} btn`}>1</button>
-                    <button className={`${baseBtn} btn`}>2</button>
-                    <button className={`${baseBtn} btn`}>3</button>
-                    <button className={`${buttonStyles.operator} operator`}>
+                    <button
+                        className={`${baseBtn} ${buttonStyles.number}`}
+                        onClick={() => handleClick('1')}
+                    >
+                        1
+                    </button>
+                    <button
+                        className={`${baseBtn} ${buttonStyles.number}`}
+                        onClick={() => handleClick('2')}
+                    >
+                        2
+                    </button>
+                    <button
+                        className={`${baseBtn} ${buttonStyles.number}`}
+                        onClick={() => handleClick('3')}
+                    >
+                        3
+                    </button>
+                    <button
+                        className={`${buttonStyles.operator} ${baseBtn}`}
+                        onClick={() => handleClick('+')}
+                    >
                         +
                     </button>
 
-                    <button className={`${baseBtn} zero col-span-2`}>0</button>
-                    <button className={`${baseBtn} btn`}>.</button>
-                    <button className={`${buttonStyles.equals} equals`}>
+                    <button
+                        className={`${baseBtn} ${buttonStyles.number} zero col-span-2`}
+                        onClick={() => handleClick('0')}
+                    >
+                        0
+                    </button>
+                    <button
+                        className={`${baseBtn} ${buttonStyles.number}`}
+                        onClick={() => handleClick('.')}
+                    >
+                        .
+                    </button>
+                    <button
+                        className={`${buttonStyles.equals} ${baseBtn}`}
+                        onClick={handleCalculate}
+                    >
                         =
                     </button>
                 </div>
